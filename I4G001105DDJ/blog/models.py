71dac02b8474f4d4b0c86ce7d16d8e4503c7ce40
Victorious-Toluwalope <victorioustolu@gmail.com>
2022-06-24 03:11:48 +0100

    Post Model

--- I4G001105DDJ/blog/models.py
+++ I4G001105DDJ/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
